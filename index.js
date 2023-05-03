ecbe2bc82281f16bdc0844140d803243d3bf5563
Aze <georgybinu247@gmail.com>
2023-05-03 09:01:21 +0530

    Updates

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const discord = require('discord.js-selfbot-v13')
 const client = new discord.Client({checkUpdate: false})
-
+const fs = require('fs');
 const consolecolor = require('gradient-string')
 
 function sleep(ms) {
@@ -40,7 +40,8 @@ let theme = [consolecolor("#f2790f", "#ff0000"), consolecolor("#00fbff", "#002af
 
 let cccv = Math.floor((Math.random() * theme.length));
 
-
+var afkStatus;
+var Afk = false;
 async function main() {
     process.title = `PHINIX ON TOP | Connected As ${client.user.username} | Guilds: ${client.guilds.cache.size}`
     let sltcv = theme[cccv]
@@ -57,7 +58,11 @@ ${sltconsole}
 
 [1] MassBan
 [2] MassKick
-[3] Exit
+[3] Custom RPC
+[4] AFK Status
+
+
+[6] Exit
     `))
 
     const e = input.question(sltcv("[?] : "))
@@ -107,7 +112,115 @@ ${sltconsole}
             })
         })
     }
-    else if (e === "3"){
+    else if (e==="3"){
+        let rpc = {};
+        let defaultRPC  = {
+            setURL: 'https://example.com',
+            setState: 'Playing',
+            setName: 'Example Game',
+            setDetails: 'Details about the game',
+            setAssetsLargeImage: 'https://media.discordapp.net/attachments/1064702481508090018/1081058743552331796/a_41d7b2a06d23c22c400f3351c3b4a15c.gif?width=160&height=160',
+            setAssetsLargeText: 'Large Image Text',
+            setAssetsSmallImage: 'https://cdn.discordapp.com/emojis/854141160623570987.gif?size=48',
+            setAssetsSmallText: 'Small Image Text',
+            button1: 'Button 1 Text',
+            button2: 'Button 2 Text',
+            button1Link : 'https://example.com',
+            button2Link : 'https://example.com'
+        };
+        try {
+            rpc = JSON.parse(fs.readFileSync('rpc.json'));
+            console.log(consolecolor("#18F952", "#18F952")(`[+] Loaded rpc.json`))
+          } catch (err) {
+            console.log(consolecolor("#FF0000", "#FF0000")(`[-] Error Loading rpc.json ${err.message}.`))
+            console.log('[?] Creating new rpc.json file...');
+            rpc = defaultRPC;
+            fs.writeFileSync('rpc.json', JSON.stringify(rpc, null, 2));
+            console.log(consolecolor("#18F952", "#18F952")(`[+] New rpc.json file created.Please fill up the configurations and re run the program`))
+            await sleep(3000)
+            process.exit(1)
+          }
+          
+          const validateString = (key) => {
+            if (typeof rpc[key] !== 'string') {
+               console.log(consolecolor("#FF0000", "#FF0000")(`[-] ${key} must be a string.`));
+                sleep(1000)
+               process.exit(1);
+            }
+          }
+          
+          validateString('setURL');
+          validateString('setState');
+          validateString('setName');
+          validateString('setDetails');
+          validateString('setAssetsLargeImage');
+          validateString('setAssetsLargeText');
+          validateString('setAssetsSmallImage');
+          validateString('setAssetsSmallText');
+          validateString('button1Link');
+          validateString('button2Link')
+          
+          
+          if (rpc.buttons) {
+            validateString('button1');
+            validateString('button2');
+          }
+          
+          const setURL = rpc.setURL;
+          const setState = rpc.setState;
+          const setName = rpc.setName;
+          const setDetails = rpc.setDetails;
+          const setAssetsLargeImage = rpc.setAssetsLargeImage;
+          const setAssetsLargeText = rpc.setAssetsLargeText;
+          const setAssetsSmallImage = rpc.setAssetsSmallImage;
+          const setAssetsSmallText = rpc.setAssetsSmallText;
+          const button1 = rpc.button1;
+          const button2 = rpc.button2;
+          const button1Link = rpc.button1Link;
+          const button2Link = rpc.button2Link
+          
+          console.log(consolecolor("#18F952", "#18F952")(`[+] rpc.json loaded and validated successfully.`));
+          bitly_axel = "https://bit.ly/axel_ontop"
+
+          const r = new discord.RichPresence()
+            .setApplicationId('1079010612769722508')
+            .setType('PLAYING') 
+            .setURL(setURL)
+            .setState(setState)
+        
+            .setName(setName)
+            .setDetails(setDetails)
+        
+        
+            .setStartTimestamp(Date.now())
+            .setAssetsLargeImage(setAssetsLargeImage)
+            .setAssetsLargeText(setAssetsLargeText)
+            .setAssetsSmallImage(setAssetsSmallImage)
+            .setAssetsSmallText(setAssetsSmallText)
+            
+            .addButton(button1, button1Link)
+            .addButton(button2, button2Link)
+        
+          client.user.setActivity(r);
+          client.user.setPresence({ status: "idle" });
+          console.log(consolecolor("#18F952", "#18F952")(`[+] Custom RPC was loaded successfully.`));
+          await sleep(2000)
+          main()
+
+    }
+    else if(e==="4"){
+        afkStatus = input.question(sltcv("[?] Your AFK Message ? : "))
+        console.log(consolecolor("#18F952", "#18F952")(`
+[+] Your afk status has been set to : ${afkStatus}.
+[!] Stop the program to disable it.`));
+
+        Afk = true
+
+    }
+    else if(e==="5"){
+
+    }
+    else if (e === "6"){
         process.exit(1)
     }
     else{
@@ -116,7 +229,31 @@ ${sltconsole}
         main()
     }
 }
+client.on('messageCreate', async (message) => {
+    if (message.author.bot) return
+    if (message.author.bot || message.author.id === client.user.id) {
+      return;
+    }
+    if(Afk==false)return;
+    if (message.channel.type === 'DM') {
+        console.log(`[+] Received DM from ${message.author.tag}: ${message.content}`);
+       await message.channel.send(
+`I'm Currently in AFK Mode.
+**Reason** : ${afkStatus}`);
+      }
+    if (message.mentions.users.has(client.user.id)) {
+        await message.channel.send(
+`I'm Currently in AFK Mode.
+**Reason** : ${afkStatus}`);  
+        console.log(`[+] Ping Recieved from ${message.author.tag}: ${message.content}.
+[+] Server Name : ${message.guild.name} `)                
+      }
+    
 
+});
 
+console.log("Loading..")
+client.on('ready', async () => { 
+    main()
 
-client.on('ready', () => main())
+})
